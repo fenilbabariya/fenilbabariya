0a8d64bbfff6bf40fa8244d577c11bf61c3d1de3
Fenil Babariya <75973998+fenilbabariya@users.noreply.github.com>
2022-09-14 11:03:30 +0530

    Update route.js

--- route.js
+++ route.js
@@ -2,9 +2,15 @@ var express = require('express')
 var router = express.Router();
 var movie = require('./Model/movie')
 
-router.get('/movies',async(req,res)=>{
-    var imovie = await movie.find();
-    res.send(imovie)
+//router.get('/movies',async(req,res)=>{
+    //var imovie = await movie.find();
+  //  res.send(imovie)
+    // res.sendFile("index.html",{root:__dirname});
+//})
+
+router.get('/photos',async(req,res)=>{
+    var iphoto = await movie.find();
+    res.send(iphoto)
     // res.sendFile("index.html",{root:__dirname});
 })
 
